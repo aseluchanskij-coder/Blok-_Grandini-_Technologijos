d60e09f40b5d0f8b446c3264a79fea6b6c753c58
aseluchanskij-coder <aseluchanskij@gmail.com>
2026-09-21 22:24:41 +0300

    padding sukurimas naudojant pirminius skaicius

--- versija_su_ai/main.cpp
+++ versija_su_ai/main.cpp
@@ -9,7 +9,6 @@ using std::cin;
 using std::endl;
 using std::string;
 
-//busima maisos funkcija
 string custom_hashas(vector<uint8_t> ivestis) {
     uint64_t state[4] = {
         0xD6D12E7B5A03A401ULL, 
@@ -17,6 +16,25 @@ string custom_hashas(vector<uint8_t> ivestis) {
         0x32A398246E20349AULL, 
         0x7158932402138901ULL  
     };
+
+    const uint8_t pirminiai[32] = {
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
+        59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131
+    };
+
+    vector<uint8_t> padded = ivestis;
+    uint64_t originalusIlgis = ivestis.size();
+
+    padded.push_back(0x80);
+
+    while ((padded.size() + 8) % 32 != 0) {
+        size_t index = padded.size() % 32;
+        padded.push_back(pirminiai[index]);
+    }
+
+    for (int i = 7; i >= 0; i--) {
+        padded.push_back((originalusIlgis >> (i * 8)) & 0xFF);
+    }
 
 
     return "LaikinasHash...";
